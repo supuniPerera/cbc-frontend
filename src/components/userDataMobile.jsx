0959b4de34efa84e05adfc8bf4d31dfc5a6ee208
supuni perera <supuniperera02@gmail.com>
2026-03-24 02:24:44 +0530

    account settings enabled ✔🖋

--- src/components/userDataMobile.jsx
+++ src/components/userDataMobile.jsx
@@ -71,7 +71,7 @@ export default function UserDataMobile(){
                     }
                 } className="ml-2 bg-accent max-w-[20px] text-white p-1 rounded">
                     <option></option>
-                    <option>Accout Settings</option>
+                    <option value="settings">Accout Settings</option>
                     <option value="logout">Logout</option>
                 
                 </select>
